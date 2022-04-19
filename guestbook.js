341b8b30cb1c1792367044ba299c09dc84dc1ccf
Andrew <45321175+Zap1999@users.noreply.github.com>
2022-04-20 01:17:25 +0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://f7fe9a75.eu-de.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
